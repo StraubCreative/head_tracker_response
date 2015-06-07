8c8a37e35bd02657a264b3740bfaa3ee38ecbd3f
erika <erika@erikasf.com>
2015-06-07 11:15:40 -0700

    Update headtrackr.js

--- headtrackr.js
+++ headtrackr.js
@@ -1090,7 +1090,23 @@ headtrackr.camshift.Rectangle = function(x,y,w,h) {
     c.x = this.x;
     c.y = this.y;
     return c;
-  }
+    var collision = function (x,y){
+    	var headx = c.x;
+    	var sidey = c.y;
+    	var butxy = function getOffset(el) {
+  		el = el.getBoundingClientRect();
+  		return {
+    			left: el.left + window.scrollX,
+    				top: el.top + window.scrollY
+				 }
+			}
+    		}
+	 }
+	 if( butxy ==  headx ) || (butxy ==s idey){
+	 	$(.call).click(){
+	 		console.log("wooot it works");
+	 	}
+	 }
 };
 
 /**
